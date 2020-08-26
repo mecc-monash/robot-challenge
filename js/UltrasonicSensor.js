5707bb5080769c187cef741203ea2e30eab8cc1c
Geordie-Tomo <58905733+Geordie-Tomo@users.noreply.github.com>
2020-08-26 16:52:25 +1000

    Directional Ultrasonics
    added a function to get the distance in every direction

--- js/UltrasonicSensor.js
+++ js/UltrasonicSensor.js
@@ -6,7 +6,7 @@ class UltrasonicSensor extends THREE.Object3D {
     constructor(car, mountPos, board, scene) {
         super();
         car.add(this);
-        this.position.copy(mountPos);
+        this.position.copy(car.position);
         this.add(new THREE.Mesh(new THREE.BoxGeometry(0.3, 0.3, 0.3), new THREE.MeshLambertMaterial({ color: 'green' })));
         this.car = car;
         this.board = board;
@@ -46,6 +46,62 @@ class UltrasonicSensor extends THREE.Object3D {
         }
         return dist;
     }
+
+    detectLeft() {
+        let dist = 100;
+        let currPos = new THREE.Vector3();
+        this.getWorldPosition(currPos);
+
+        let from = new THREE.Vector3(currPos.x,1,currPos.z);
+        let direction = new THREE.Vector3(1,0,0).applyAxisAngle(new THREE.Vector3(0,1,0), this.car.rotation.y);
+
+        raycaster.set(from, direction);
+        
+        var raycast = raycaster.intersectObjects(this.board.obstacles);
+        if (raycast.length)
+        {
+            dist = raycast[0].distance;
+        }
+        return dist;
+    }
+
+    detectRight() {
+        let dist = 100;
+        let currPos = new THREE.Vector3();
+        this.getWorldPosition(currPos);
+
+        let from = new THREE.Vector3(currPos.x,1,currPos.z);
+        let direction = new THREE.Vector3(-1,0,0).applyAxisAngle(new THREE.Vector3(0,1,0), this.car.rotation.y);
+
+        raycaster.set(from, direction);
+        
+        var raycast = raycaster.intersectObjects(this.board.obstacles);
+        if (raycast.length)
+        {
+            dist = raycast[0].distance;
+        }
+        return dist;
+    }
+
+    detectBackwards() {
+        let dist = 100;
+        let currPos = new THREE.Vector3();
+        this.getWorldPosition(currPos);
+
+        let from = new THREE.Vector3(currPos.x,1,currPos.z);
+        let direction = new THREE.Vector3(0,0,-1).applyAxisAngle(new THREE.Vector3(0,1,0), this.car.rotation.y);
+
+        raycaster.set(from, direction);
+        
+        var raycast = raycaster.intersectObjects(this.board.obstacles);
+        if (raycast.length)
+        {
+            dist = raycast[0].distance;
+        }
+        return dist;
+    }
+
+    
 }
 
 export default UltrasonicSensor;
