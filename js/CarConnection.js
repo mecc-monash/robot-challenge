42b36df9e5cfd6e33e8840b05aeab9a379a926a1
owenbrooks <owen.h.brooks@gmail.com>
2020-08-30 10:42:26 +1000

    Remove randomness when speed is set to zero

--- js/CarConnection.js
+++ js/CarConnection.js
@@ -3,12 +3,20 @@ class CarConnection {
     constructor(car) {
         this.car = car;
     }
-    
+
     setSpeedA(newSpeed) {
-        this.car.diffSpeed.a = newSpeed + Math.floor(Math.random() * 31);
+        if (newSpeed === 0) {
+            this.car.diffSpeed.a = 0; // don't add randomisation for zero speed
+        } else {
+            this.car.diffSpeed.a = newSpeed + Math.floor(Math.random() * 31);
+        }
     }
     setSpeedB(newSpeed) {
-        this.car.diffSpeed.b = newSpeed + Math.floor(Math.random() * 31);;
+        if (newSpeed === 0) {
+            this.car.diffSpeed.b = 0; // don't add randomisation for zero speed
+        } else {
+            this.car.diffSpeed.b = newSpeed + Math.floor(Math.random() * 31);
+        }
     }
 }
 
